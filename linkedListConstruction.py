abe267552f7f5ee2070c036106dd4f11a815d2b8
Goutham Krishna <goutham.sagam@gmail.com>
2020-09-22 19:07:07 -0400

    linkedList

---
+++ linkedListConstruction.py
@@ -0,0 +1,99 @@
+# Feel free to add new properties and methods to the class.
+class Node:
+	def __init__(self,value):
+		self.value = value
+		self.prev = None
+		self.next = None
+		
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def setHead(self, node):
+        # Write your code here.
+		if self.head is None:
+			self.head = Node
+			self.tail = Node
+			return
+		self.insertBefore(self.head,node)
+
+    def setTail(self, node):
+        # Write your code here.
+		if self.tail is None:
+			self.setHead(node)
+			return
+		self.insertAfter(self.tail, node)
+		#self.remove(node)
+		
+
+    def insertBefore(self, node, nodeToInsert):
+        # Write your code here.
+		
+		
+
+    def insertAfter(self, node, nodeToInsert):
+        # Write your code here.
+		
+
+    def insertAtPosition(self, position, nodeToInsert):
+        # Write your code here.
+		if position == 1:
+			self.setHead(nodeToInsert)
+			return
+		node = self.head
+		cur = 1
+		while node is not None and cur != position:
+			node = node.next
+			cur +=1
+		if node is not None:
+			self.insertBefore(node,nodeToInsert)
+		else:
+			self.setTail(nodeToInsert)
+			
+		
+		
+		
+
+    def removeNodesWithValue(self, value):
+        # Write your code here.
+		node = self.head
+		while node is not None:
+			nodeToRemove = node
+			node = node.next
+			if nodeToRemove.value = value:
+				self.remove(nodeToRemove)
+		
+
+    def remove(self, node):
+        # Write your code here.
+		if node == self.head:
+			self.head = self.head.next
+		if node == self.tail:
+			self.tail = self.tail.prev
+		self.removeNodeBindings(node)
+	
+	def removeNodeBindings(self,node):
+		if node.prev is not None:
+			node.prev.next = node.next
+		
+		if node.next is not None:
+			node.next.prev = node.prev
+		
+		node.prev = None
+		node.next = None
+		
+	
+			
+
+    def containsNodeWithValue(self, value):
+		cur = self.head
+		while cur != None:
+			if cur.value == value:
+				return True
+			cur = cur.next
+		return False
+			
+			
+	
